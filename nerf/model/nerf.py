3677728315090a7cc157bc6e2488007113895364
Marsen <lamisgosu11@gmail.com>
2024-01-18 18:29:00 +0700

    updates for nerf runners

--- nerf/model/nerf.py
+++ nerf/model/nerf.py
@@ -1,5 +1,5 @@
-import numpy as np
 import torch
+import numpy as np
 from tqdm import tqdm
 import torch.nn as nn
 import matplotlib.pyplot as plt
@@ -80,3 +80,70 @@ def render_rays(nerf_model, ray_origins, ray_direction, hn=0, hf=0.5, nb_bins=19
     alpha = 1 - torch.exp(-sigma * delta)
     weights = compute_accumulated_transmittance(
         1-alpha).unsqueeze(2)*alpha.unsqueeze(2)
+    c = (weights*colors).sum(1)
+    weight_sum = weights.sum(-1).sum(-1)
+    return c + 1 - weight_sum.unsqueeze(-1)
+
+
+def train(nerf_model, optimizer, scheduler, data_loader, device='cpu', hn=0, hf=1, nb_epochs=int(1e5),
+          nb_bins=192, H=400, W=400):
+    training_loss = []
+    for _ in tqdm(range(nb_epochs)):
+        for batch in data_loader:
+            ray_origins = batch[:, :3].to(device)
+            ray_directions = batch[:, 3:6].to(device)
+            ground_truth_px_values = batch[:, 6:].to(device)
+
+            regenerated_px_values = render_rays(
+                nerf_model, ray_origins, ray_directions, hn=hn, hf=hf, nb_bins=nb_bins)
+            loss = ((ground_truth_px_values - regenerated_px_values) ** 2).sum()
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            training_loss.append(loss.item())
+        scheduler.step()
+
+        for img_index in range(200):
+            test(hn, hf, testing_dataset, img_index=img_index,
+                 nb_bins=nb_bins, H=H, W=W)
+    return training_loss
+
+
+@torch.no_grad()
+def test(hn, hf, dataset, chunk_size=10, img_index=0, nb_bins=192, H=400, W=400):
+    ray_origins = dataset[img_index * H * W: (img_index + 1) * H * W, :3]
+    ray_directions = dataset[img_index * H * W: (img_index + 1) * H * W, 3:6]
+
+    data = []   # list of regenerated pixel values
+    for i in range(int(np.ceil(H / chunk_size))):   # iterate over chunks
+        # Get chunk of rays
+        ray_origins_ = ray_origins[i * W *
+                                   chunk_size: (i + 1) * W * chunk_size].to(device)
+        ray_directions_ = ray_directions[i * W *
+                                         chunk_size: (i + 1) * W * chunk_size].to(device)
+        regenerated_px_values = render_rays(
+            model, ray_origins_, ray_directions_, hn=hn, hf=hf, nb_bins=nb_bins)
+        data.append(regenerated_px_values)
+    img = torch.cat(data).data.cpu().numpy().reshape(H, W, 3)
+
+    plt.figure()
+    plt.imshow(img)
+    plt.savefig(f'novel_views/img_{img_index}.png', bbox_inches='tight')
+    plt.close()
+
+
+if __name__ == '__main__':
+    device = 'cuda'
+
+    training_dataset = torch.from_numpy(
+        np.load('training_data.pkl', allow_pickle=True))
+    testing_dataset = torch.from_numpy(
+        np.load('testing_data.pkl', allow_pickle=True))
+    model = NerfModel(hidden_dim=256).to(device)
+    model_optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(
+        model_optimizer, milestones=[2, 4, 8], gamma=0.5)
+    data_loader = DataLoader(training_dataset, batch_size=1024, shuffle=True)
+    train(model, model_optimizer, scheduler, data_loader, nb_epochs=16, device=device, hn=2, hf=6, nb_bins=192, H=400,
+          W=400)
